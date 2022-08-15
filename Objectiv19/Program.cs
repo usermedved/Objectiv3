c7b847420e20fe60788373815ff649b81c5bcd6f
Sergei Medvedev <clip1988@yandex.ru>
2022-08-15 23:17:49 +0300

    Objectiv 19 project

---
+++ Objectiv19/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+if (number[0] == number[4])
+{
+   if(number[1] == number[3]);
+   {
+    Console.WriteLine($"Является палиндромом {number}");
+   }
+}
+else Console.WriteLine($"Не является палиндромом {number}");
