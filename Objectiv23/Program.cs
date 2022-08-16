cb2b3288ab3c003a26ff65f90e05fe913634b043
Sergei Medvedev <clip1988@yandex.ru>
2022-08-16 10:19:58 +0300

    Objectiv 23 project

---
+++ Objectiv23/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int cof = 1; cof <= number; cof++)
+{
+    Console.WriteLine($"Возведение в куб {cof}: {Math.Pow(cof,3)}");
+}
